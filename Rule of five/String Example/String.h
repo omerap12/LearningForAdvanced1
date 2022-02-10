d063b4afb322e86c4629e04579f37f2111680822
Omer Aplatony <omerap12@gmail.com>
2022-02-10 17:10:21 +0200

    Rule of five

---
+++ Rule of five/String Example/String.h
@@ -0,0 +1,98 @@
+//
+// Created by omera on 10/02/2022.
+//
+
+#ifndef LEARNINGFORTEST_STRING_H
+#define LEARNINGFORTEST_STRING_H
+
+#include <iostream>
+#include <cstring>
+
+class String {
+private:
+    char* chars;
+    int len;
+public:
+    /**
+     * constructor.
+     * @param other - the other char*.
+     */
+    String(const char* other){
+        this->len =  strlen(other);
+        this->chars = new char[len];
+        strcpy(this->chars, other);
+    }
+
+    /**
+     * destructor
+     */
+    ~String(){
+        delete[] chars;
+    }
+
+    /**
+     * copy assignment operator
+     * @param s - the other string.
+     * @return - same object address
+     */
+    String& operator = (const String& s){
+        if (this != &s){
+            delete[] chars;
+            this->len = s.len;
+            chars = new char[this->len];
+            strcpy(this->chars,s.chars);
+        }
+        return *this;
+    }
+
+
+    /**
+     * Copy constructor.
+     * @param s - the other String.
+     */
+    String(const String& s){
+        this->chars = nullptr;
+        //calling the copy assignment operator
+        *this = s;
+    }
+
+    /**
+     * Move assignment operator.
+     * @param s - the other string.
+     * @return - same object address
+     */
+    String& operator = (String&& s){
+        if (this != &s){
+            this->len = s.len;
+            this->chars = s.chars;
+            s.len = 0;
+            s.chars = nullptr;
+        }
+        return *this;
+    }
+
+    /**
+     * move constructor - calling the move assignment operator
+     * @param s - the String we need to move - && means it's temporary object. It's destructor will call himself.
+     */
+    String(String&&  s){
+        *this = std::move(s);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+
+#endif //LEARNINGFORTEST_STRING_H
---
+++ Rule of five/String Example/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "String.h"
+#include "Employee.h"
+
+
+int main() {
+
+    //using move constructor
+    Employee e(std::move("omer"));
+
+
+
+}
